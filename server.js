6ef1e5b5829db535011d6780cbd86dfbf4a77a97
Lilopadilla22 <lilopadilla22@gmail.com>
2025-04-28 21:38:50 -0500

    update server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3100;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
