bff74be070038e7a3871c33b8e66c28f069279b4
Lilopadilla22 <lilopadilla22@gmail.com>
2025-05-01 18:10:11 -0500

    update app

--- app.js
+++ app.js
@@ -15,8 +15,20 @@ app.use(cookieParser());
 
 const authRoutes = require('./src/routes/authRoutes');
 app.use('/api', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Servidor de Distribuidora funcionando 🚀');
 });
+
+const categoryRoutes = require('./src/routes/categoryRoutes');
+app.use('/api/categorias', categoryRoutes);
+
+const productRoutes = require('./src/routes/productRoutes');
+app.use('/api/productos', productRoutes);
+
+const orderRoutes = require('./src/routes/orderRoutes');
+app.use('/api/pedidos', orderRoutes);
+
+
 
 module.exports = app;
