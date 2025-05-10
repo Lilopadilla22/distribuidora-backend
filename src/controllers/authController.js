9f87f887e482bbf011f495854945da0fffb037b6
lilia.padilla <lilia.padilla@beex.ar>
2025-05-10 17:12:02 -0500

    update controller auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,6 +15,7 @@ const registerUser = async (req, res) => {
       fecha_cumpleanos,
       cc,
       nombre_negocio,
+      role,
     } = req.body;
 
     const userExists = await User.findOne({ email });
@@ -35,6 +36,7 @@ const registerUser = async (req, res) => {
       fecha_cumpleanos,
       cc,
       nombre_negocio,
+      role: role || 'client'
     });
 
     const userToReturn = { ...newUser._doc };
