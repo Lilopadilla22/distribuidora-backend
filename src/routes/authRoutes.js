92487fdbacdb3206213de5fb4babb4532a105827
LiliaPadilla22 <lilopadilla22@gmail.com>
2025-08-16 15:57:19 -0500

    add new routes auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getProfile } = require('../controllers/authController');
+const { registerUser, loginUser, getProfile, obtenerUsuarios } = require('../controllers/authController');
 const verifyToken = require('../middlewares/authMiddleware');
+const isAdmin = require('../middlewares/isAdmin')
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', verifyToken, getProfile);
+router.get('/usuarios', verifyToken, isAdmin, obtenerUsuarios )
 
 module.exports = router;
